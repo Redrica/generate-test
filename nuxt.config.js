79c06ccfc7dda9c0712bbd9c3c6de06296731084
Elizaveta Vinokurova <de.spring@gmail.com>
2021-07-07 22:35:16 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
     // Target: https://go.nuxtjs.dev/config-target
     target: 'static',
     router: {
-        base: 'generate-test',
+        base: '/generate-test/',
     },
 
     // Global page headers: https://go.nuxtjs.dev/config-head
